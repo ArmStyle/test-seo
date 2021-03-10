40e500787f2bc838803a8ee4f414dce8ea5fb055
Siriwut.n <siriwut.n@cdg.co.th>
2021-03-11 00:35:56 +0700

    no message

---
+++ pages/manga/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-layout wrap class="mt-9 mx-0">
+    <v-flex xs12 sm12 md12 lg9 class="py-3">มังงะ</v-flex>
+    <v-flex xs12 sm12 md12 lg3 class="py-3"></v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: `มังงะ `,
+      meta: [
+        { charset: 'utf-8' },
+        {
+          hid: 'viewport',
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1'
+        },
+        {
+          hid: 'twitter:card',
+          name: 'twitter:card',
+          content: 'read cartoon '
+        },
+        { hid: 'twitter:site', name: 'twitter:site', content: '@7toons' },
+        { hid: 'twitter:creator', name: 'twitter:creator', content: '@7toons' },
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: `มังงะ `
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: `อ่านมังงะแปลไทย และ อื่นๆอีกมากมาย `
+        },
+        {
+          hid: 'twitter:image',
+          name: 'twitter:image',
+          content: `/icon.png`
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: `อ่านมังงะแปลไทย และ อื่นๆอีกมากมาย `
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: `/icon.png`
+        },
+        {
+          hid: 'og:site_name',
+          name: 'og:site_name',
+          content: `มังงะ `
+        },
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: `มังงะ  `
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          content: `อ่านมังงะแปลไทย และ อื่นๆอีกมากมาย `
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>

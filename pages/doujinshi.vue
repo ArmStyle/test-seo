31a20f582b16084e142fcf4e2f51723907d4a015
Siriwut.n <siriwut.n@cdg.co.th>
2021-03-13 22:08:41 +0700

    no message

---
+++ pages/doujinshi.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-layout wrap class="mt-9 mx-0">
+    <v-flex xs12 sm12 md12 lg9 class="py-3">
+      Fo0bo=b
+    </v-flex>
+    <v-flex xs12 sm12 md12 lg3 class="py-3">
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+// import myFunc from '../../js/myFunction.js'
+export default {
+  data() {
+    return {
+      // myFunc,
+    }
+  },
+  head() {
+    return {
+      title: `โดจินชิ`,
+      meta: [
+        { charset: 'utf-8' },
+        {
+          hid: 'viewport',
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1',
+        },
+        {
+          hid: 'twitter:card',
+          name: 'twitter:card',
+          content: 'read cartoon ',
+        },
+        { hid: 'twitter:site', name: 'twitter:site', content: '@7toons' },
+        { hid: 'twitter:creator', name: 'twitter:creator', content: '@7toons' },
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: `โดจินชิ `,
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: `อ่านโดจินชิแปลไทย และ อื่นๆอีกมากมาย `,
+        },
+        {
+          hid: 'twitter:image',
+          name: 'twitter:image',
+          content: `/icon.png`,
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: `อ่านโดจินชิแปลไทย และ อื่นๆอีกมากมาย `,
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: `/icon.png`,
+        },
+        {
+          hid: 'og:site_name',
+          name: 'og:site_name',
+          content: `โดจินชิ `,
+        },
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: `โดจินชิ  `,
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          content: `อ่านโดจินชิแปลไทย และ อื่นๆอีกมากมาย `,
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>

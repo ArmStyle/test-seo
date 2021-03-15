d3d19c4bd3c28fbad43b8a2b9b9a264db3fbac6e
Siriwut.n <siriwut.n@cdg.co.th>
2021-03-15 10:38:10 +0700

    update cartoon title

--- pages/cartoon/_title/index.vue
+++ pages/cartoon/_title/index.vue
@@ -1,137 +1,133 @@
 <template>
-<v-container class="mt-12" style="max-width: 1180px">
-  <div>
-    <v-row class="detail">
-      <v-col cols="12" lg="9">
-        <v-container class="px-3">
-          <v-layout class="detail-box mb-3" wrap>
-            <v-layout class="pa-1" wrap>
-              <v-flex xs12 md4>
-                <v-img class="mx-auto detail-img" :src="$store.state.cartoon.thumbnail"></v-img>
-              </v-flex>
-              <v-flex xs12 md8 class="detail-text my-auto">
-                <v-layout row>
-                  <v-flex xs12 md12>
-                    <v-layout row warp>
-                      <div
-                        v-for="(item, i) in $store.state.cartoon.category"
-                        :key="i"
-                        class="hover-primary"
-                      >
-                        <v-layout
-                          nowrap
-                          class="pt-1"
-                          @click="
-                              setCategory($store.state.cartoon.genre, item)
+  <v-container class="mt-12" style="max-width: 1180px">
+    <div>
+      <v-row class="detail">
+        <v-col cols="12" lg="9">
+          <v-container class="px-3">
+            <v-layout class="detail-box mb-3" wrap>
+              <v-layout class="pa-1" wrap>
+                <v-flex xs12 md4>
+                  <v-img class="mx-auto detail-img" :src="cartoon.thumbnail"></v-img>
+                </v-flex>
+                <v-flex xs12 md8 class="detail-text my-auto">
+                  <v-layout row>
+                    <v-flex xs12 md12>
+                      <v-layout row warp>
+                        <div v-for="(item, i) in cartoon.category" :key="i" class="hover-primary">
+                          <v-layout
+                            nowrap
+                            class="pt-1"
+                            @click="
+                              setCategory(cartoon.genre, item)
                             "
-                        >
-                          <nuxt-link :to="`/` + $store.state.cartoon.genre">
-                            <h3>{{ item }}</h3>
-                          </nuxt-link>
-                          <h3
-                            v-if="
-                                i != $store.state.cartoon.category.length - 1
+                          >
+                            <nuxt-link :to="`/` + cartoon.genre">
+                              <h3>{{ item }}</h3>
+                            </nuxt-link>
+                            <h3
+                              v-if="
+                                i != cartoon.category.length - 1
                               "
-                            class="space"
-                          >|</h3>
-                        </v-layout>
-                      </div>
-                    </v-layout>
-                    <h1>{{ $store.state.cartoon.title }}</h1>
-                    <h3>{{ $store.state.cartoon.artist }}</h3>
-                    <v-layout nowarp>
-                      <v-rating></v-rating>
-                      <h4 style="padding-top: 0.25rem">({{ $store.state.cartoon.score }})</h4>
-                    </v-layout>
-                    <h5 class="max-lines">{{ $store.state.cartoon.summary }}</h5>
-                    <nuxt-link
-                      :to="
-                          ($store.state.cartoon.id
-                            ? $store.state.cartoon.id.replace('CT', '')
-                            : $store.state.cartoon.id) +
+                              class="space"
+                            >|</h3>
+                          </v-layout>
+                        </div>
+                      </v-layout>
+                      <h1>{{ cartoon.title }}</h1>
+                      <h3>{{ cartoon.artist }}</h3>
+                      <v-layout nowarp>
+                        <v-rating></v-rating>
+                        <h4 style="padding-top: 0.25rem">({{ cartoon.score }})</h4>
+                      </v-layout>
+                      <h5 class="max-lines">{{ cartoon.summary }}</h5>
+                      <nuxt-link
+                        :to="
+                          (cartoon.id
+                            ? cartoon.id.replace('CT', '')
+                            : cartoon.id) +
                           `/` +
-                          ($store.state.cartoon.episodes
-                            ? $store.state.cartoon.episodes[0].episode
+                          (cartoon.episodes
+                            ? cartoon.episodes[0].episode
                             : '')
                         "
-                    >
-                      <v-btn
-                        class="detail-btn"
-                        v-if="$store.state.cartoon.episodes"
-                        outlined
-                        rounded
-                        @click="setEp($store.state.cartoon.episodes[0])"
-                      >ไปตอนแรกสุด</v-btn>
-                    </nuxt-link>
-                  </v-flex>
-                </v-layout>
-              </v-flex>
+                      >
+                        <v-btn
+                          class="detail-btn"
+                          v-if="cartoon.episodes"
+                          outlined
+                          rounded
+                          @click="setEp(cartoon.episodes[0])"
+                        >ไปตอนแรกสุด</v-btn>
+                      </nuxt-link>
+                    </v-flex>
+                  </v-layout>
+                </v-flex>
+              </v-layout>
             </v-layout>
-          </v-layout>
-          <Title title="อัพเดตล่าสุด !" class="mt-10 pa-0" />
-          <v-flex xs12 v-if="showLess">
-            <v-flex xs12 v-for="(item, i) in cartoonEp.slice(0, 10)" :key="i">
-              <ul @click="setEp(item)">
-                <li>
-                  <nuxt-link
-                    :to="
-                        ($store.state.cartoon.id
-                          ? $store.state.cartoon.id.replace('CT', '')
-                          : $store.state.cartoon.id) +
+            <Title title="อัพเดตล่าสุด !" class="mt-10 pa-0" />
+            <v-flex xs12 v-if="showLess">
+              <v-flex xs12 v-for="(item, i) in cartoonEp.slice(0, 10)" :key="i">
+                <ul @click="setEp(item)">
+                  <li>
+                    <nuxt-link
+                      :to="
+                        (cartoon.id
+                          ? cartoon.id.replace('CT', '')
+                          : cartoon.id) +
                         `/` +
                         item.episode
                       "
-                    style="line-height: 50px"
-                  >
-                    <span>
-                      ตอนที่ {{ item.episode }}
-                      {{
-                      $store.state.cartoon.status == 'end' && i == 0
-                      ? 'จบ'
-                      : ''
-                      }}
-                    </span>
-                    <v-spacer></v-spacer>
-                    <h4 style="color: gray">{{ item.created | formatDate }}</h4>
-                  </nuxt-link>
-                </li>
-              </ul>
+                      style="line-height: 50px"
+                    >
+                      <span>
+                        ตอนที่ {{ item.episode }}
+                        {{
+                        cartoon.status == 'end' && i == 0
+                        ? 'จบ'
+                        : ''
+                        }}
+                      </span>
+                      <v-spacer></v-spacer>
+                      <h4 style="color: gray">{{ item.created | formatDate }}</h4>
+                    </nuxt-link>
+                  </li>
+                </ul>
+              </v-flex>
+              <v-flex xs12 v-if="showLess && cartoonEp.length > 10" class="pt-3">
+                <v-btn @click="showLess = false" block text>show more</v-btn>
+              </v-flex>
             </v-flex>
-            <v-flex xs12 v-if="showLess && cartoonEp.length > 10" class="pt-3">
-              <v-btn @click="showLess = false" block text>show more</v-btn>
-            </v-flex>
-          </v-flex>
-          <v-flex xs12 v-else>
-            <v-flex xs12 v-for="(item, i) in cartoonEp" :key="i">
-              <ul @click="setEp(item)">
-                <li>
-                  <nuxt-link
-                    :to="
-                        ($store.state.cartoon.id
-                          ? $store.state.cartoon.id.replace('CT', '')
-                          : $store.state.cartoon.id) +
+            <v-flex xs12 v-else>
+              <v-flex xs12 v-for="(item, i) in cartoonEp" :key="i">
+                <ul @click="setEp(item)">
+                  <li>
+                    <nuxt-link
+                      :to="
+                        (cartoon.id
+                          ? cartoon.id.replace('CT', '')
+                          : cartoon.id) +
                         `/` +
                         item.episode
                       "
-                    style="line-height: 50px"
-                  >
-                    <span>ตอนที่ {{ item.episode }}</span>
-                    <v-spacer></v-spacer>
-                    <span style="color: gray">{{ item.created | formatDate }}</span>
-                  </nuxt-link>
-                </li>
-              </ul>
+                      style="line-height: 50px"
+                    >
+                      <span>ตอนที่ {{ item.episode }}</span>
+                      <v-spacer></v-spacer>
+                      <span style="color: gray">{{ item.created | formatDate }}</span>
+                    </nuxt-link>
+                  </li>
+                </ul>
+              </v-flex>
             </v-flex>
-          </v-flex>
-          <!-- <MainCard /> -->
-        </v-container>
-      </v-col>
-      <v-col cols="12" lg="3">
-        <SideCard />
-      </v-col>
-    </v-row>
-  </div>
-</v-container>
+            <!-- <MainCard /> -->
+          </v-container>
+        </v-col>
+        <v-col cols="12" lg="3">
+          <SideCard />
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
 </template>
 
 <script>
@@ -141,10 +137,7 @@ export default {
   async asyncData(ctx) {
     let cartoonId = 'CT' + ctx.route.params.title
     let getCartoon = await ctx.$axios.get(`/cartoon/` + cartoonId, {})
-    let getEpisodes = await ctx.$axios.get(
-      '/episode?cartoon_id=' + cartoonId,
-      {}
-    )
+    let getEpisodes = await ctx.$axios.get('/episode?cartoon_id=' + cartoonId, {})
     return { getCartoon: getCartoon.data, getEpisodes: getEpisodes.data }
   },
   head() {
@@ -155,80 +148,78 @@ export default {
         {
           hid: 'viewport',
           name: 'viewport',
-          content: 'width=device-width, initial-scale=1',
+          content: 'width=device-width, initial-scale=1'
         },
         {
           hid: 'twitter:card',
           name: 'twitter:card',
-          content: 'read cartoon ',
+          content: 'read cartoon '
         },
         { hid: 'twitter:site', name: 'twitter:site', content: '@7toons' },
         { hid: 'twitter:creator', name: 'twitter:creator', content: '@7toons' },
         {
           hid: 'twitter:title',
           name: 'twitter:title',
-          content: `${this.getCartoon.title} `,
+          content: `${this.getCartoon.title} `
         },
         {
           hid: 'twitter:description',
           name: 'twitter:description',
-          content: this.getCartoon.summary,
+          content: this.getCartoon.summary
         },
         {
           hid: 'twitter:image',
           name: 'twitter:image',
-          content: this.getCartoon.thumbnail,
+          content: this.getCartoon.thumbnail
         },
         {
           hid: 'description',
           name: 'description',
-          content: this.getCartoon.summary,
+          content: this.getCartoon.summary
         },
         {
           hid: 'og:image',
           property: 'og:image',
-          content: this.getCartoon.thumbnail,
+          content: this.getCartoon.thumbnail
         },
         {
           hid: 'og:site_name',
           name: 'og:site_name',
-          content: `${this.getCartoon.title} `,
+          content: `${this.getCartoon.title} `
         },
         {
           hid: 'og:title',
           name: 'og:title',
-          content: `${this.getCartoon.title}  `,
+          content: `${this.getCartoon.title}  `
         },
         {
           hid: 'og:description',
           name: 'og:description',
-          content: this.getCartoon.summary,
-        },
-      ],
+          content: this.getCartoon.summary
+        }
+      ]
     }
   },
 
   data() {
     return {
-      showLess: true,
+      showLess: true
     }
   },
   computed: {
     ...mapState({
-      listCartoonsPopular: (state) => state.listCartoonsPopular,
-      cartoon: (state) => state.cartoon,
-      cartoonEp: (state) => state.cartoonEp,
-    }),
+      listCartoonsPopular: state => state.listCartoonsPopular,
+      cartoon: state => state.cartoon,
+      cartoonEp: state => state.cartoonEp
+    })
   },
-  async created() {
-    this.$nuxt.$loading.start()
-    this.getCartoon.category = await this.checkForDuplicates(
-      this.getCartoon.category
-    )
-    document.querySelector('#infinite-list').scrollTop = 0
+  async mounted() {
+    // this.$nuxt.$loading.start()
+    this.getCartoon.category = await this.checkForDuplicates(this.getCartoon.category)
+    // document.querySelector('#infinite-list').scrollTop = 0
     this.$store.commit('SET_CARTOON', this.getCartoon)
     this.$store.commit('SET_CARTOON_EP', this.getEpisodes)
-    this.$nuxt.$loading.finish()
+    // this.$nuxt.$loading.finish()
   },
   methods: {
     setCategory(genre, category) {
@@ -241,10 +232,7 @@ export default {
         }
       }
       if (genre == 'doujin') {
-        this.$store.commit(
-          'SET_TAB',
-          this.$store.state.listCategoryDoujin.indexOf(category)
-        )
+        this.$store.commit('SET_TAB', this.$store.state.listCategoryDoujin.indexOf(category))
       }
       this.getListCartoon()
     },
@@ -270,8 +258,7 @@ export default {
     async getListCartoon() {
       this.toTop()
       let genre = this.$store.state.genre
-      let category =
-        this.$store.state.category != 'all' ? this.$store.state.category : null
+      let category = this.$store.state.category != 'all' ? this.$store.state.category : null
       let listCartoons
 
       if (!genre || genre == 'index') {
@@ -279,10 +266,7 @@ export default {
           listCartoons = await this.$axios.$get('/cartoon/latest?limit=20', {})
           this.$store.commit('SET_LIST_CARTOONS', listCartoons.data)
           if (listCartoons.LastEvaluatedKey) {
-            this.$store.commit(
-              'SET_LASTEVALUATEDKEY',
-              listCartoons.LastEvaluatedKey
-            )
+            this.$store.commit('SET_LASTEVALUATEDKEY', listCartoons.LastEvaluatedKey)
           } else {
             this.$store.commit('SET_LASTEVALUATEDKEY', null)
           }
@@ -290,26 +274,18 @@ export default {
       } else {
         try {
           let api =
-            `/cartoon/filter?limit=20&` +
-            (genre ? 'genre=' + genre : '') +
-            (genre && category ? '&' : '') +
-            (category ? 'category=' + category : '')
+            `/cartoon/filter?limit=20&` + (genre ? 'genre=' + genre : '') + (genre && category ? '&' : '') + (category ? 'category=' + category : '')
 
           listCartoons = await this.$axios.get(api, {})
           this.$store.commit('SET_LIST_CARTOONS', listCartoons.data)
           if (listCartoons.data.LastEvaluatedKey) {
-            this.$store.commit(
-              'SET_LASTEVALUATEDKEY',
-              listCartoons.data.LastEvaluatedKey
-            )
+            this.$store.commit('SET_LASTEVALUATEDKEY', listCartoons.data.LastEvaluatedKey)
           } else {
             this.$store.commit('SET_LASTEVALUATEDKEY', null)
           }
         } catch (error) {}
       }
-    },
-  },
+    }
+  }
 }
 </script>
-
-<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,30 +2,30 @@
   <v-layout wrap class="mt-9 mx-0">
     <v-flex xs12 sm12 md12 lg9 class="py-3">
       <HomeCard
-        v-if="$store.state.listManga.length != 0"
+        v-if="listManga.length != 0"
         title="มังงะอัพเดตล่าสุด !"
-        :listCartoons="$store.state.listManga"
+        :listCartoons="listManga"
         :showAll="true"
         route="/manga"
       />
       <HomeCard
-        v-if="$store.state.listNovel.length != 0"
+        v-if="listNovel.length != 0"
         title="นิยายอัพเดตล่าสุด !"
-        :listCartoons="$store.state.listNovel"
+        :listCartoons="listNovel"
         :showAll="true"
         route="/novel"
       />
       <HomeCard
-        v-if="$store.state.listDoujinshi.length != 0"
+        v-if="listDoujinshi.length != 0"
         title="โดจินชิอัพเดตล่าสุด !"
-        :listCartoons="$store.state.listDoujinshi"
+        :listCartoons="listDoujinshi"
         :showAll="true"
         route="/doujinshi"
       />
       <HomeCard
-        v-if="$store.state.listDoujin.length != 0"
+        v-if="listDoujin.length != 0"
         title="โดจินอัพเดตล่าสุด !"
-        :listCartoons="$store.state.listDoujin"
+        :listCartoons="listDoujin"
         :showAll="true"
         route="/doujin"
       />
@@ -38,6 +38,8 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   head() {
     return {
@@ -47,57 +49,68 @@ export default {
         {
           hid: 'viewport',
           name: 'viewport',
-          content: 'width=device-width, initial-scale=1',
+          content: 'width=device-width, initial-scale=1'
         },
         {
           hid: 'twitter:card',
           name: 'twitter:card',
-          content: 'read cartoon ',
+          content: 'read cartoon '
         },
         { hid: 'twitter:site', name: 'twitter:site', content: '@7toons' },
         { hid: 'twitter:creator', name: 'twitter:creator', content: '@7toons' },
         {
           hid: 'twitter:title',
           name: 'twitter:title',
-          content: `อ่านการ์ตูนออนไลน์ `,
+          content: `อ่านการ์ตูนออนไลน์ `
         },
         {
           hid: 'twitter:description',
           name: 'twitter:description',
-          content: `อ่านการ์ตูนแปลไทย 24 ชั่วโมง เว็บอ่านการ์ตูนมีทุกประเภท ผลงานแปล Manga (ญี่ปุ่น), Manhwa (เกาหลี), จีนและ Comic (ตะวันตก) , Doujin (R18+) • ผลงานวาด Doujin (ศัพท์ญี่ปุ่น) ทุกหมวดหมู่ อัพเดทการ์ตูนทุกวัน เว็บอ่านการ์ตูนออนไลน์.`,
+          content: `อ่านการ์ตูนแปลไทย 24 ชั่วโมง เว็บอ่านการ์ตูนมีทุกประเภท ผลงานแปล Manga (ญี่ปุ่น), Manhwa (เกาหลี), จีนและ Comic (ตะวันตก) , Doujin (R18+) • ผลงานวาด Doujin (ศัพท์ญี่ปุ่น) ทุกหมวดหมู่ อัพเดทการ์ตูนทุกวัน เว็บอ่านการ์ตูนออนไลน์.`
         },
         {
           hid: 'twitter:image',
           name: 'twitter:image',
-          content: `/icon.png`,
+          content: `/icon.png`
         },
         {
           hid: 'description',
           name: 'description',
-          content: `อ่านการ์ตูนแปลไทย 24 ชั่วโมง เว็บอ่านการ์ตูนมีทุกประเภท ผลงานแปล Manga (ญี่ปุ่น), Manhwa (เกาหลี), จีนและ Comic (ตะวันตก) , Doujin (R18+) • ผลงานวาด Doujin (ศัพท์ญี่ปุ่น) ทุกหมวดหมู่ อัพเดทการ์ตูนทุกวัน เว็บอ่านการ์ตูนออนไลน์.`,
+          content: `อ่านการ์ตูนแปลไทย 24 ชั่วโมง เว็บอ่านการ์ตูนมีทุกประเภท ผลงานแปล Manga (ญี่ปุ่น), Manhwa (เกาหลี), จีนและ Comic (ตะวันตก) , Doujin (R18+) • ผลงานวาด Doujin (ศัพท์ญี่ปุ่น) ทุกหมวดหมู่ อัพเดทการ์ตูนทุกวัน เว็บอ่านการ์ตูนออนไลน์.`
         },
         {
           hid: 'og:image',
           property: 'og:image',
-          content: `/icon.png`,
+          content: `/icon.png`
         },
         {
           hid: 'og:site_name',
           name: 'og:site_name',
-          content: `อ่านการ์ตูนออนไลน์ `,
+          content: `อ่านการ์ตูนออนไลน์ `
         },
         {
           hid: 'og:title',
           name: 'og:title',
-          content: `อ่านการ์ตูนออนไลน์ `,
+          content: `อ่านการ์ตูนออนไลน์ `
         },
         {
           hid: 'og:description',
           name: 'og:description',
-          content: `อ่านการ์ตูนแปลไทย 24 ชั่วโมง เว็บอ่านการ์ตูนมีทุกประเภท ผลงานแปล Manga (ญี่ปุ่น), Manhwa (เกาหลี), จีนและ Comic (ตะวันตก) , Doujin (R18+) • ผลงานวาด Doujin (ศัพท์ญี่ปุ่น) ทุกหมวดหมู่ อัพเดทการ์ตูนทุกวัน เว็บอ่านการ์ตูนออนไลน์.`,
-        },
-      ],
+          content: `อ่านการ์ตูนแปลไทย 24 ชั่วโมง เว็บอ่านการ์ตูนมีทุกประเภท ผลงานแปล Manga (ญี่ปุ่น), Manhwa (เกาหลี), จีนและ Comic (ตะวันตก) , Doujin (R18+) • ผลงานวาด Doujin (ศัพท์ญี่ปุ่น) ทุกหมวดหมู่ อัพเดทการ์ตูนทุกวัน เว็บอ่านการ์ตูนออนไลน์.`
+        }
+      ]
     }
   },
+  computed: {
+    ...mapState({
+      listManga: state => state.listManga,
+      listNovel: state => state.listNovel,
+      listDoujinshi: state => state.listDoujinshi,
+      listDoujin: state => state.listDoujin
+    })
+  }
 }
 </script>
+
+<style scoped>
+</style>
